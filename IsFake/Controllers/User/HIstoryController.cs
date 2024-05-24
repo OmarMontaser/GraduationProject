d1d737f63c70c58a844bed5e3a9047a2f0a78d17
Montaser <omarmontaser34@gmail.com>
2024-05-24 11:56:49 +0300

    updated

---
+++ IsFake/Controllers/User/HIstoryController.cs
@@ -0,0 +1,61 @@
+﻿using IsFakeModels;
+using IsFakeRepository;
+using IsFakeViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IsFake.Controllers.User
+{
+    public class HIstoryController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        public readonly UserStatement _userStatement;
+        public readonly UserRecord _userRecord;
+        public readonly UserManager<ApplicationUser> _userManager;
+       // public readonly IdentityUser _identityUser;
+        public HIstoryController(ApplicationDbContext context,
+                              UserStatement userStatement,
+                              UserRecord userRecord,
+                              UserManager<ApplicationUser> userManager
+                             // IdentityUser identityUser
+                                )
+        {
+            _context = context;
+            _userStatement = userStatement;
+            _userRecord = userRecord;
+            _userManager = userManager;
+           // _identityUser = identityUser;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> History()
+        {
+            ApplicationUser currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                // Handle the case where the current user is not found
+                return NotFound();
+            }
+
+            // Fetch user records from UserStatement and UserRecord tables
+            var userStatements = _context.UserStatements.Where(UserStatement => UserStatement.ApplicationUserId == currentUser.Id);
+            var userRecords    = _context.UserRecords.Where(UserRecord => UserRecord.ApplicationUserId == currentUser.Id);
+
+            // Construct a view model to pass data to the view
+            var viewModel = new HIstoryViewModel
+            {
+               // Id = currentUser.Id,
+                UserName = currentUser.UserName,
+                StatementFile = userStatements.ToList(), // Assuming StatementFile and VoiceFile are lists
+                VoiceFile = userRecords.ToList()
+            };
+
+            return View(viewModel);
+        }
+
+
+    }
+}
--- IsFake/obj/Debug/net7.0/IsFake.AssemblyInfo.cs
+++ IsFake/obj/Debug/net7.0/IsFake.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IsFake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f86b666576fd5ec5d833ba1153c46b9fd03c3a94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b90850b6e90d224ba59b82f30b4c15d6b65c2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IsFake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IsFake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
