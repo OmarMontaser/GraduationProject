653dd7bc8390e6db1fa9e06e35f147ba9c7ee5d3
Montaser <omarmontaser34@gmail.com>
2024-06-02 20:41:24 +0300

    Add Controller

--- IsFake/obj/Release/net7.0/IsFake.AssemblyInfo.cs
+++ IsFake/obj/Release/net7.0/IsFake.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IsFake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aab634f6f1f41c96d18950b874a29e3d86935fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b471b4450495e0dc6261ded67713ef38a39b419")]
 [assembly: System.Reflection.AssemblyProductAttribute("IsFake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IsFake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
